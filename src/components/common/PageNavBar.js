a94f6477eebd9fba4a8f2b09951f0641d846c205
Rossana87 <rossana.ventrella@gmail.com>
2023-03-10 08:55:58 +0000

    logo and some css

--- src/components/common/PageNavBar.js
+++ src/components/common/PageNavBar.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/Navbar'
 import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
+import Logo from '../../images/harvard-art-museums-full.jpeg'
 
 const PageNavBar = () => {
   return (
     <Navbar expand="md">
       <Container>
-        <Navbar.Brand to="/" as={Link}>Logo</Navbar.Brand>
+        <Navbar.Brand to="/" as={Link} className='logo'><img src={Logo} /></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
           <Nav>
--- src/components/gallery/GallerySingle.js
+++ src/components/gallery/GallerySingle.js
@@ -11,7 +11,7 @@ import Col from 'react-bootstrap/Col'
 
 const GallerySingle = () => {
 
-  const [art, setArt] = useState([])
+  const [art, setArt] = useState(null)
 
   const apiKey = 'ea65da40-779b-43c2-87a7-1bbc03fef7d6&size=100&page=10'
 
@@ -37,13 +37,13 @@ const GallerySingle = () => {
     <main>
       <Container>
         <Row>
-          {art && art.length > 0 &&
+          {art &&
             <>
               <Col xs="12">
-                <h1 className='display-4 mb-4'>{art.name}</h1>
+                <h1 className='display-6 mb-4'>{art.people[name]}</h1>
               </Col>
               <Col md="6">
-                <img src={art.baseimageurl} alt="" />
+                <img key={id} src={art.images.baseimageurl} alt="" />
               </Col>
               <Col md="6">
                 <h4></h4>
